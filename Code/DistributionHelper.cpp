820add377e2fd199283c7f84717b334a8c9a1992
Alexander Neumann <alexander.neumann@hamburg.de>
2016-10-20 22:46:10 +0200

    Compiles but strange problems with temporary std::string
    Former-commit-id: b1f95af5b729afbf2e1e62eb59ac9bb440d245ae
    Former-commit-id: 7cbcb4f80886f899669e6d726e8eb1e0f6f96a1a
    Former-commit-id: 7bd3b316feebeed855221fda080514b6e269096b

--- Code/DistributionHelper.cpp
+++ Code/DistributionHelper.cpp
@@ -1,21 +1,21 @@
 #include "..\Headers\DistributionHelper.h"
 
-std::string to_string(const IDistribution& field)
-{
-	return IDistributionMap.at(field);
-};
+//std::string to_string(const IDistribution& field)
+//{
+//	return IDistributionMap.at(field);
+//};
 
-template<>
-IDistribution from_string<IDistribution>(const std::string &String)
-{
-	for (auto it : IDistributionMap)
-		if (it.second == String)
-			return it.first;
-
-	throw std::runtime_error{ std::string{ "Type of Distribution unknown! " } +String };
-};
-
-template IDistribution from_string<IDistribution>(const std::string &String);
+//template<>
+//IDistribution from_string<IDistribution>(const std::string &String)
+//{
+//	for (auto it : IDistributionMap)
+//		if (it.second == String)
+//			return it.first;
+//
+//	throw std::runtime_error{ std::string{ "Type of Distribution unknown! " } +String };
+//};
+//
+//template IDistribution from_string<IDistribution>(const std::string &String);
 
 //
 //namespace
--- Headers/DistributionHelper.h
+++ Headers/DistributionHelper.h
@@ -23,100 +23,106 @@
 
 #include "Logger.h"
 
-
-
-enum class IDistribution { Distribution_unknown, Distribution_delta, Distribution_normal, Distribution_lognormal };
-static const std::map<IDistribution, std::string> IDistributionMap{ { { IDistribution::Distribution_unknown,"unknown" },{ IDistribution::Distribution_delta,"delta" } ,{ IDistribution::Distribution_normal,"normal" },{ IDistribution::Distribution_lognormal,"lognormal" } } };
-
-std::string to_string(const IDistribution& field);
-
-template<typename T>
-T from_string(const std::string &);
-//template<>
-//inline IDistribution from_string<IDistribution>(const std::string &String) //TODO: Remoe inline move to cpp; currently gives linker errors!
-//{
-//	for (auto it : IDistributionMap)
-//		if (it.second == String)
-//			return it.first;
-//
-//	throw std::runtime_error{ std::string{ "Type of Distribution unknown! " } +String };
-//};
-
-template<typename ReturnValue>
-class IDistributionHelper
+namespace Distribution
 {
-	MY_VIRTUAL_INTERFACE(IDistributionHelper)
-public:
-	virtual ReturnValue getValueFromDistribution() = 0;
-};
 
-template<typename prec>
-class DeltaDistribution : public IDistributionHelper<prec>
-{
-private:
-	prec deltavalue;
-public:
-	DeltaDistribution(const prec& value) : deltavalue(value) {};
-	prec getValueFromDistribution() override final { return deltavalue; };
+	enum class IDistribution { Distribution_unknown, Distribution_delta, Distribution_normal, Distribution_lognormal };
+	static const std::map<IDistribution, std::string> IDistributionMap{ { { IDistribution::Distribution_unknown,"unknown" },{ IDistribution::Distribution_delta,"delta" } ,{ IDistribution::Distribution_normal,"normal" },{ IDistribution::Distribution_lognormal,"lognormal" } } };
 
-};
-
-template <typename prec, typename distribution >
-class DistributionHelper : public IDistributionHelper<prec>
-{
-private:
-	typedef typename std::pair<prec, prec> DistributionParams;
-
-	distribution _distribution;
-	std::mt19937_64 _prng;
-public:
-	//TODO: use better init for PNG see noisefield!
-	template <typename T = distribution>
-	DistributionHelper(const prec &value, typename std::enable_if_t<std::is_same<T, std::discrete_distribution<prec>>::value, prec>* = nullptr)
-		:_distribution(distribution{ value }), _prng(std::random_device{}())
+	inline std::string to_string(const IDistribution& field)
 	{
-		_prng.discard(10'000'000);
-	}
-	DistributionHelper(const DistributionParams &values)
-		: _distribution(distribution{ values.first, values.second }),
-		_prng(std::random_device{}())
-	{
-		_prng.discard(10'000'000);
+		return IDistributionMap.at(field);
 	};
 
-	prec getValueFromDistribution() override final
+	template<typename T>
+	T from_string(const std::string &);
+	template<>
+	inline IDistribution from_string<IDistribution>(const std::string &String) //TODO: Remoe inline move to cpp; currently gives linker errors!
 	{
-		return _distribution(_prng);
-	}
+		for (auto it : IDistributionMap)
+			if (it.second == String)
+				return it.first;
+
+		throw std::runtime_error{ std::string{ "Type of Distribution unknown! " } +String };
+	};
+
+	template<typename ReturnValue>
+	class IDistributionHelper
+	{
+		MY_VIRTUAL_INTERFACE(IDistributionHelper)
+	public:
+		virtual ReturnValue getValueFromDistribution() = 0;
+	};
+
+	template<typename prec>
+	class DeltaDistribution : public IDistributionHelper<prec>
+	{
+	private:
+		prec deltavalue;
+	public:
+		DeltaDistribution(const prec& value) : deltavalue(value) {};
+		prec getValueFromDistribution() override final { return deltavalue; };
+
+	};
+
+	template <typename prec, typename distribution >
+	class DistributionHelper : public IDistributionHelper<prec>
+	{
+	private:
+		typedef typename std::pair<prec, prec> DistributionParams;
+
+		distribution _distribution;
+		std::mt19937_64 _prng;
+	public:
+		//TODO: use better init for PNG see noisefield!
+		template <typename T = distribution>
+		DistributionHelper(const prec &value, typename std::enable_if_t<std::is_same<T, std::discrete_distribution<prec>>::value, prec>* = nullptr)
+			:_distribution(distribution{ value }), _prng(std::random_device{}())
+		{
+			_prng.discard(10'000'000);
+		}
+		DistributionHelper(const DistributionParams &values)
+			: _distribution(distribution{ values.first, values.second }),
+			_prng(std::random_device{}())
+		{
+			_prng.discard(10'000'000);
+		};
+
+		prec getValueFromDistribution() override final
+		{
+			return _distribution(_prng);
+		}
 
 
+
+	};
+
+	template <typename inttype>
+	class DistributionHelperDiscrete : public IDistributionHelper<inttype>
+	{
+		static_assert(std::is_integral<inttype>::value, "This class is only for integral types!");
+	private:
+		std::discrete_distribution<inttype> _distribution;
+		std::mt19937_64 _prng;
+
+	protected:
+
+	public:
+		//TODO: use better init for PNG see noisefield!
+		//TODO: Silence stupid compiler warning due to implicit conversion from inttype to double!
+		DistributionHelperDiscrete(const std::vector<inttype>& values)
+			:_distribution(values.begin(), values.end()), _prng((std::random_device{})())
+		{
+			_prng.discard(10'000'000);
+		}
+		inttype getValueFromDistribution() override final
+		{
+			return _distribution(_prng);
+		}
+
+	};
 
 };
-
-template <typename inttype>
-class DistributionHelperDiscrete : public IDistributionHelper<inttype>
-{
-	static_assert(std::is_integral<inttype>::value, "This class is only for integral types!");
-private:
-	std::discrete_distribution<inttype> _distribution;
-	std::mt19937_64 _prng;
-
-protected:
-
-public:
-	//TODO: use better init for PNG see noisefield!
-	DistributionHelperDiscrete(const std::vector<inttype>& values)
-		:_distribution( values.begin(),values.end() ), _prng((std::random_device{})())
-	{
-		_prng.discard(10'000'000);
-	}
-	inttype getValueFromDistribution() override final
-	{
-		return _distribution(_prng);
-	}
-
-};
-
 #endif	// INC_DistributionHelper_H
 // end of DistributionHelper.h
 ///---------------------------------------------------------------------------------------------------
