6c9550ae0674128caff74de77c0b1ac247d45f0c
Alexander Neumann <alexander.neumann@hamburg.de>
2017-07-18 18:08:04 +0200

    test implicit

--- math/Geometry.h
+++ math/Geometry.h
@@ -17,27 +17,29 @@
 #include <type_traits>
 #include <utility>
 
-#ifndef M_PI
-#define M_PI   3.1415926535897932384626433832795
-#endif
-#ifndef M_1_PI
-#define M_1_PI 0.31830988618379067153776752674503
-#endif
 
-// Two underscroes __{var} means 1/{var}
-#define M_4PI      12.566370614359172953850573533118   // 4 * PI 
+
+namespace math::geometry
+{
+	template<typename T>
+	extern constexpr T math_pi() { return static_cast<T>(3.1415926535897932384626433832795); };
+	
+	template<typename T>
+	extern constexpr T math_1_pi() { return static_cast<T>(0.31830988618379067153776752674503); };
+
+	// Two underscroes __{var} means 1/{var}
+#define M_PI		3.1415926535897932384626433832795
+#define M_4PI		12.566370614359172953850573533118;   // 4 * PI
 #define M__4PI      0.07957747154594766788444188168626   // 1/4PI 
 #define M_2PI       6.283185307179586476925286766559   // 2 * PI 
 #define M__2PI      0.15915494309189533576888376337251   // 1/2PI 
 #define M_4PI_3     4.1887902047863909846168578443727   // 4 * PI / 3
 #define M_3__4PI    0.23873241463784300365332564505877   // 3 / (PI * 4)
 
-namespace math::geometry
-{
 	namespace sphere
 	{
 		template<typename precision>
-		inline static constexpr precision calcSurfaceArea(const precision& radius) noexcept { return M_4PI*std::pow(radius, 2); };
+		inline static constexpr precision calcSurfaceArea(const precision& radius) noexcept { return math_pi<precision>*std::pow(radius, 2); };
 		template<typename precision>
 		inline static constexpr precision calcRadiusFromSurfaceArea(const precision& area) noexcept { return std::sqrt(area*M__4PI); };
 
@@ -70,9 +72,9 @@ namespace math::geometry
 	namespace circle
 	{
 		template<typename precision>
-		inline static constexpr precision calcArea(const precision& radius) noexcept { return M_PI*std::pow(radius, 2); };
+		inline static constexpr precision calcArea(const precision& radius) noexcept { return  math_pi<precision>()*std::pow(radius, 2); };
 		template<typename precision>
-		inline static constexpr precision calcRadiusArea(const precision& area) noexcept { return std::sqrt(area*M_1_PI, 2); };
+		inline static constexpr precision calcRadiusArea(const precision& area) noexcept { return std::sqrt(area*math_1_pi<precision>(), 2); };
 
 		template<typename precision>
 		inline static constexpr precision calcCircumference(const precision& radius) noexcept { return M_2PI*radius; };
