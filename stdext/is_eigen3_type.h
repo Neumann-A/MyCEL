339b61b1d49b301e30c2fcfa251e9268a59b191a
Alexander Neumann <Alexander.Neumann@hamburg.de>
2017-08-08 23:35:47 +0200

    trying to get Tensors to play nicely with matlab

--- stdext/is_eigen3_type.h
+++ stdext/is_eigen3_type.h
@@ -15,16 +15,18 @@
 
 #include <type_traits>
 
+#include <utility>
+
 #include "is_container.h"
 
 namespace Eigen
 {
-#ifndef EIGEN_CORE_H
-	template<typename Derived> struct EigenBase;
-#endif
-#ifndef EIGEN_CXX11_TENSOR_TENSOR_H
-	template<typename Derived, int AccessLevel = 0> class TensorBase;
-#endif
+//#ifndef EIGEN_CORE_H
+//	template<typename Derived> struct EigenBase;
+//#endif
+//#ifndef EIGEN_CXX11_TENSOR_TENSOR_H
+//	template<typename Derived, int AccessLevel = 0> class TensorBase;
+//#endif
 }
 
 namespace stdext
@@ -32,11 +34,13 @@ namespace stdext
 	template<typename T, typename _ = std::void_t<>>
 	struct is_eigen_type : std::false_type {};
 
-
-
 	template<typename T, typename _ = std::void_t<>>
 	struct is_container_with_eigen_t : std::false_type {};
+
+	template<typename T, typename _ = std::void_t<>>
+	struct is_eigen_tensor : std::false_type {};
 	
+#ifdef EIGEN_CORE_H
 	template<typename T>
 	struct is_eigen_type<T> : std::is_base_of<Eigen::EigenBase<std::decay_t<T>>, std::decay_t<T>> {};
 	
@@ -51,21 +55,22 @@ namespace stdext
 
 	template<typename T>
 	static constexpr bool is_container_with_eigen_type_v = is_container_with_eigen_t<T>::value;
-
-
-	template<typename T, typename _ = std::void_t<>>
-	struct is_eigen_tensor : std::false_type {};
-
+	
+#ifdef EIGEN_CXX11_TENSOR_TENSOR_H
 	template<typename T>
-	struct is_eigen_tensor<T> : std::is_base_of<Eigen::TensorBase<T>, T> {};
+	struct is_eigen_tensor<T, std::void_t<typename T::Scalar, decltype(std::declval<T>().NumIndices) >> : std::is_base_of<Eigen::TensorBase<T>, T> {};
 
 	template<typename T>
 	static constexpr bool is_eigen_tensor_v = is_eigen_tensor<T>::value;
 
 	namespace
 	{
-		//static_assert(is_eigen_tensor_v<Eigen::Tensor<double, 2>>);
+
+		static_assert(is_eigen_tensor_v<Eigen::Tensor<double, 2>>);
+
 	}
+#endif
+#endif
 }
 
 #endif	// INC_is_eigen3_type_H
