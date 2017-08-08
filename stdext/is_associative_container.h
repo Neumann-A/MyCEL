fb60c6f630c9effaf58c2f275bb1f580c850193b
Alexander Neumann <alexander.neumann@hamburg.de>
2017-08-08 15:39:41 +0200

    bugfixes

--- stdext/is_associative_container.h
+++ stdext/is_associative_container.h
@@ -14,4 +14,30 @@ namespace stdext
 
 	template<typename T>
 	static constexpr bool is_associative_container_v = is_associative_container<T>::value;
+
+	namespace
+	{
+		static_assert(!is_associative_container_v<std::array<double, 5>>);
+		static_assert(!is_associative_container_v<std::vector<float>>);
+		static_assert(!is_associative_container_v<std::deque<int>>);
+		static_assert(!is_associative_container_v<std::list<std::uint16_t>>);
+
+		//
+		static_assert(is_associative_container_v<std::set<float>>);
+		static_assert(is_associative_container_v<std::multiset<float>>);
+		static_assert(is_associative_container_v<std::map<std::uint16_t, float>>);
+		static_assert(is_associative_container_v<std::multimap<std::uint16_t, float>>);
+		static_assert(is_associative_container_v<std::multiset<float>>);
+		static_assert(is_associative_container_v<std::unordered_set<float>>);
+		static_assert(is_associative_container_v<std::unordered_multiset<float>>);
+		static_assert(is_associative_container_v<std::unordered_map<std::uint16_t, float>>);
+		static_assert(is_associative_container_v<std::unordered_multimap<std::uint16_t, float>>);
+
+		//These are not containers due to lack of some iterators or members! (They use one of the above internally but have a different interface!)
+		static_assert(!is_associative_container_v<std::forward_list<float>>); //Lacks size() method !
+		static_assert(!is_associative_container_v<std::stack<float>>);
+		static_assert(!is_associative_container_v<std::queue<float>>);
+		static_assert(!is_associative_container_v<std::priority_queue<float>>);
+	}
+
 }
