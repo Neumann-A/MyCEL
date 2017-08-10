dbae51d9098d6572eb51c5aeaf2ce5cada457d11
Alexander Neumann <alexander.neumann@hamburg.de>
2017-08-10 16:35:50 +0200

    HDF5 Archive. There seems to be in issue with VS code generation in DEBUG mode with move only types! -> Have to check it.

--- stdext/is_eigen3_type.h
+++ stdext/is_eigen3_type.h
@@ -40,16 +40,19 @@ namespace stdext
 	template<typename T, typename _ = std::void_t<>>
 	struct is_eigen_tensor : std::false_type {};
 	
+	template<typename T>
+	static constexpr bool is_eigen_type_v = is_eigen_type<T>::value;
+
+	template<typename T>
+	static constexpr bool is_eigen_tensor_v = is_eigen_tensor<T>::value;
+
 #ifdef EIGEN_CORE_H
 	template<typename T>
 	struct is_eigen_type<T> : std::is_base_of<Eigen::EigenBase<std::decay_t<T>>, std::decay_t<T>> {};
 	
 	//template<typename T>
 	//struct is_eigen_tensor_type<T> : std::is_base_of<Eigen::TensorBase<std::decay_t<T>>, std::decay_t<T>> {};
-
-	template<typename T>
-	static constexpr bool is_eigen_type_v = is_eigen_type<T>::value;
-
+	
 	template<typename T>
 	struct is_container_with_eigen_t<T, std::void_t<typename std::decay_t<T>::value_type>> : std::conjunction<stdext::is_container<std::decay_t<T>>, is_eigen_type<typename std::decay_t<T>::value_type>> {};
 
@@ -60,8 +63,7 @@ namespace stdext
 	template<typename T>
 	struct is_eigen_tensor<T, std::void_t<typename T::Scalar, decltype(std::declval<T>().NumIndices) >> : std::is_base_of<Eigen::TensorBase<T>, T> {};
 
-	template<typename T>
-	static constexpr bool is_eigen_tensor_v = is_eigen_tensor<T>::value;
+
 
 	namespace
 	{
