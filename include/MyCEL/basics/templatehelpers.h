0c2df21d01222827c6c71ac7108b9789d7034e4a
Alexander Neumann <alexander.neumann@hamburg.de>
2020-11-27 16:42:44 +0100

    use size member instead of size free function

--- include/MyCEL/basics/templatehelpers.h
+++ include/MyCEL/basics/templatehelpers.h
@@ -10,10 +10,10 @@ namespace MyCEL {
     constexpr auto apply_nttp_impl(std::index_sequence<Is...>) -> Output<get<Is>(Input)...>;
 
     template <const auto &Input, template <std::remove_cvref_t<decltype(get<0>(Input))>...> typename Output>
-    constexpr auto apply_nttp() -> decltype(apply_nttp_impl<Input,Output>(std::make_index_sequence<size(Input)>()));
+    constexpr auto apply_nttp() -> decltype(apply_nttp_impl<Input,Output>(std::make_index_sequence<Input.size()>()));
 
     template <const auto &Input, template <std::remove_cvref_t<decltype(get<0>(Input))>...> typename Output>
-    using apply_nttp_t = decltype(apply_nttp_impl<Input, Output>(std::make_index_sequence<size(Input)>()));
+    using apply_nttp_t = decltype(apply_nttp_impl<Input, Output>(std::make_index_sequence<Input.size()>()));
 }
 
 #endif //INC_MYCEL_TEMPLATEHELPERS_H
