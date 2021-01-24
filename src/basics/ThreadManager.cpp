612f1f544f72c26b362c1e6f7b494047a59c6beb
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-01-24 19:24:03 +0100

    fix init order

--- src/basics/ThreadManager.cpp
+++ src/basics/ThreadManager.cpp
@@ -125,7 +125,7 @@ void ThreadManager::Log(const std::string& msg)
 }
 
 ThreadManager::ThreadManager(const std::uint64_t NumberOfThreadsToCreate) 
-    : _tid(std::this_thread::get_id()), _Threads(), _NumberOfThreads(createThreads(NumberOfThreadsToCreate)), _Tasks() {}
+    : _TaskDequeMutex(), _TaskCondVar(), _Threads(), _tid(std::this_thread::get_id()), _NumberOfThreads(createThreads(NumberOfThreadsToCreate)), _Tasks() {}
 ThreadManager::~ThreadManager()
 {
 #ifdef _DEBUG
