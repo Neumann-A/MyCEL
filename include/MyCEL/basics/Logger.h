0fdedaa7a4db42d2ee7bc58570ad470d50b82fd7
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-13 21:39:30 +0100

    fix indent

--- include/MyCEL/basics/Logger.h
+++ include/MyCEL/basics/Logger.h
@@ -12,22 +12,22 @@
 class Logger
 {
 private:
-	Logger() = default;
+    Logger() = default;
 protected:
-	
+    
 public:
-	// In the near future when fold expressions are a thing
-	//template<typename... Args>
-	//inline static void Log(Args&&... args)
-	//{
-	//	std::stringstream str;
-	//	str << ... << args ;
-	//	Log(str);
-	//}
+    // In the near future when fold expressions are a thing
+    //template<typename... Args>
+    //inline static void Log(Args&&... args)
+    //{
+    //	std::stringstream str;
+    //	str << ... << args ;
+    //	Log(str);
+    //}
 
-	inline static void Log(std::string_view msg) 
-	{ 
-		////TODO: Implement Time as an extra
+    inline static void Log(std::string_view msg) 
+    { 
+        ////TODO: Implement Time as an extra
         //  if (msg.back() == '\n')
         // {
         std::cout << msg << std::flush;
@@ -38,13 +38,13 @@ public:
         //    std::string fixasync{ std::string{msg} +'\n' };
         //    std::cout << fixasync;
         //}
-	}
-	inline static void Log(const std::stringstream &msg) 
-	{ 
-		Log(msg.str());
-	}
-	inline static void Log(const char* fmt, ...)
-	{
+    }
+    inline static void Log(const std::stringstream &msg) 
+    { 
+        Log(msg.str());
+    }
+    inline static void Log(const char* fmt, ...)
+    {
 #ifdef _MSC_VER
 #pragma warning( push )
 #pragma warning( disable : 6386 )
@@ -53,39 +53,39 @@ public:
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wformat-nonliteral"
 #endif
-		const int size = 512;
-		char* buffer = new char[size];
-		va_list vl;
-		va_start(vl, fmt);
-		int nsize = vsnprintf(buffer, size, fmt, vl);
-		if (size <= nsize)
-		{	//fail delete buffer and try again
-			delete[] buffer;
-			buffer = new char[static_cast<size_t>(nsize) + 1]; //+1 for /0;
-			nsize = vsnprintf(buffer, size, fmt, vl);
-		}
-		std::string ret{ buffer };
-		va_end(vl);
-		delete[] buffer;
-		Log(ret);
+        const int size = 512;
+        char* buffer = new char[size];
+        va_list vl;
+        va_start(vl, fmt);
+        int nsize = vsnprintf(buffer, size, fmt, vl);
+        if (size <= nsize)
+        {	//fail delete buffer and try again
+            delete[] buffer;
+            buffer = new char[static_cast<size_t>(nsize) + 1]; //+1 for /0;
+            nsize = vsnprintf(buffer, size, fmt, vl);
+        }
+        std::string ret{ buffer };
+        va_end(vl);
+        delete[] buffer;
+        Log(ret);
 #ifdef __llvm__
 #pragma clang diagnostic pop
 #endif
 #ifdef _MSC_VER
 #pragma warning( pop )
 #endif
-	}
+    }
 
-	inline static void Error(const std::string &msg)
-	{
-		std::cerr << msg << std::endl;
-	}
-	inline static void Error(const std::stringstream &msg)
-	{
-		Error(msg.str());
-	}
-	inline static void Error(const char* fmt, ...)
-	{
+    inline static void Error(const std::string &msg)
+    {
+        std::cerr << msg << std::endl;
+    }
+    inline static void Error(const std::stringstream &msg)
+    {
+        Error(msg.str());
+    }
+    inline static void Error(const char* fmt, ...)
+    {
 #ifdef _MSC_VER
 #pragma warning( push )
 #pragma warning( disable : 6386 )
@@ -94,31 +94,31 @@ public:
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wformat-nonliteral"
 #endif
-		const int size = 512;
-		char* buffer = new char[size];
-		va_list vl;
-		va_start(vl, fmt);
-		int nsize = vsnprintf(buffer, size, fmt, vl);
-		if (size <= nsize)
-		{	//fail delete buffer and try again
-			delete[] buffer;
-			buffer = new char[static_cast<size_t>(nsize) + 1]; //+1 for /0;
-			nsize = vsnprintf(buffer, size, fmt, vl);
-		}
-		std::string ret{ buffer };
-		va_end(vl);
-		delete[] buffer;
-		Error(ret);
+        const int size = 512;
+        char* buffer = new char[size];
+        va_list vl;
+        va_start(vl, fmt);
+        int nsize = vsnprintf(buffer, size, fmt, vl);
+        if (size <= nsize)
+        {	//fail delete buffer and try again
+            delete[] buffer;
+            buffer = new char[static_cast<size_t>(nsize) + 1]; //+1 for /0;
+            nsize = vsnprintf(buffer, size, fmt, vl);
+        }
+        std::string ret{ buffer };
+        va_end(vl);
+        delete[] buffer;
+        Error(ret);
 #ifdef __llvm__
 #pragma clang diagnostic pop
 #endif
 #ifdef _MSC_VER
 #pragma warning( pop )
 #endif
-	}
+    }
 
-	inline static std::string Format(const char* fmt, ...)
-	{
+    inline static std::string Format(const char* fmt, ...)
+    {
 #ifdef _MSC_VER
 #pragma warning( push )
 #pragma warning( disable : 6386 )
@@ -127,28 +127,28 @@ public:
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wformat-nonliteral"
 #endif
-		const int size = 512;
-		char* buffer = new char[size];
-		va_list vl;
-		va_start(vl, fmt);
-		int nsize = vsnprintf(buffer, size, fmt, vl);
-		if (size <= nsize)
-		{	//fail delete buffer and try again
-			delete[] buffer;
-			//buffer = 0;
-			buffer = new char[static_cast<size_t>(nsize) + 1]; //+1 for /0
-			nsize = vsnprintf(buffer, size, fmt, vl);
-		}
-		std::string ret{ buffer };
-		va_end(vl);
-		delete[] buffer;
-		return ret;
+        const int size = 512;
+        char* buffer = new char[size];
+        va_list vl;
+        va_start(vl, fmt);
+        int nsize = vsnprintf(buffer, size, fmt, vl);
+        if (size <= nsize)
+        {	//fail delete buffer and try again
+            delete[] buffer;
+            //buffer = 0;
+            buffer = new char[static_cast<size_t>(nsize) + 1]; //+1 for /0
+            nsize = vsnprintf(buffer, size, fmt, vl);
+        }
+        std::string ret{ buffer };
+        va_end(vl);
+        delete[] buffer;
+        return ret;
 #ifdef __llvm__
 #pragma clang diagnostic pop
 #endif
 #ifdef _MSC_VER
 #pragma warning( pop )
 #endif
-	}
+    }
 
 };
