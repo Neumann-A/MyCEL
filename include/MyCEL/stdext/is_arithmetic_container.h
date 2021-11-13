b9edad9d90e7e4af530d53401f8d07c0bfe8faeb
Alexander Neumann <Alexander.Neumann@hamburg.de>
2021-11-13 21:39:47 +0100

    is remove_cvref_t instead of decay

--- include/MyCEL/stdext/is_arithmetic_container.h
+++ include/MyCEL/stdext/is_arithmetic_container.h
@@ -27,9 +27,9 @@ namespace stdext
 
     template <typename T>
     struct is_arithmetic_container<T,
-        std::void_t< typename std::decay_t<T>::value_type >> : std::conjunction <  is_container< std::decay_t<T> >,
+        std::void_t< typename std::remove_cvref_t<T>::value_type >> : std::conjunction <  is_container< std::remove_cvref_t<T> >,
                                                         std::negation< is_string<T> >,
-        std::is_arithmetic< typename std::decay_t<T>::value_type > > {};
+        std::is_arithmetic< typename std::remove_cvref_t<T>::value_type > > {};
     template<typename T>
     static constexpr bool is_arithmetic_container_v = is_arithmetic_container<T>::value;
 
